c9b54100b28544a2ca2d19cc8a37f8ddec7562de
Asigan <Zacharycgrippo@gmail.com>
2021-09-20 16:38:43 -0500

    Initial setup

---
+++ Shop/Game.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    struct item
+    {
+        public string name;
+        public int cost;
+    }
+    class Game
+    {
+        private string player;
+        private string shop;
+        private bool gameOver;
+        private int currentScene;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+
+        private void InitializeItems()
+        {
+
+        }
+
+        void save()
+        {
+
+        }
+
+        bool load()
+        {
+
+        }
+
+        void DisplayCurrentScene()
+        {
+
+        }
+
+        void DisplayOpeningMenu()
+        {
+
+        }
+
+        string[]GetShopMenuOptions()
+        {
+
+        }
+
+        void DisplayShopMenu()
+        {
+
+        }
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ Shop/Shop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    class Shop
+    {
+        int gold;
+        item[] _inventory;
+
+
+        
+    }
+}
